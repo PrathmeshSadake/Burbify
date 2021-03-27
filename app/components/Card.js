d797fc61aecf00017277c3fff9fa5136ac2ce416
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-03-28 00:28:13 +0530

    created card and list-item compoennt

---
+++ app/components/Card.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
+
+import AppText from "./AppText";
+import colors from "../config/colors";
+
+function Card({ title, subTitle, image }) {
+  return (
+    <View style={styles.card}>
+      <Image style={styles.image} source={image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 12,
+    backgroundColor: colors.white,
+    marginBottom: 20,
+    overflow: "hidden",
+  },
+  detailsContainer: {
+    padding: 15,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  subTitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+  },
+  title: {
+    marginBottom: 5,
+  },
+});
+
+export default Card;
