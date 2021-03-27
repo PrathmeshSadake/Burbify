b067a0c76c4cd01a1966ad536b16080cdd26f4c3
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-03-28 00:28:27 +0530

    rendering view-image-screen

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { View } from "react-native";
+
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
