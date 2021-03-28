8c85ae971bc100b5f99a07bd0fcf27034d64105b
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-03-29 01:17:24 +0530

    created reusable list item separator

---
+++ app/components/ListItemSeparator.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import colors from "../config/colors";
+
+function ListItemSeparator(props) {
+  return <View style={styles.keySeparator} />;
+}
+
+const styles = StyleSheet.create({
+  keySeparator: {
+    width: "100%",
+    height: 2,
+    backgroundColor: colors.lightGrey,
+  },
+});
+
+export default ListItemSeparator;
