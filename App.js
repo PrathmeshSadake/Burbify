3bc250343789e12c42a63f84296e631310c4608f
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-03-29 01:16:53 +0530

    Rendering ViewImage screen

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
