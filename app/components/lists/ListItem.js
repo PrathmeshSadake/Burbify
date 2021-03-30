6985ff271a17b9c9e10be035b9ee0326537fee67
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-03-30 23:33:26 +0530

    created separate component folder for forms and lists. moved all files related to froms and list in respective folder

--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -2,8 +2,8 @@ import React from "react";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
 
-import colors from "../config/colors";
-import AppText from "./AppText";
+import colors from "../../config/colors";
+import AppText from "../AppText";
 
 function ListItem({
   title,
